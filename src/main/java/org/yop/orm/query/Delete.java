79af8617e0153c9305d060c9d3caff1f060e407d
hdubourg <hdubourg@gmail.com>
2018-02-21 12:45:16 +0000

    [ORM] [API] [UPSERT/DELETE] Upsert and delete classes. No implementation yet !

---
+++ src/main/java/org/yop/orm/query/Delete.java
@@ -0,0 +1,11 @@
+package org.yop.orm.query;
+
+import org.yop.orm.model.Yopable;
+
+/**
+ * Delete : delete instances of T from the database.
+ *
+ * @param <T> the type to delete.
+ */
+public class Delete<T extends Yopable> {
+}
---
+++ src/main/java/org/yop/orm/query/Upsert.java
@@ -0,0 +1,11 @@
+package org.yop.orm.query;
+
+import org.yop.orm.model.Yopable;
+
+/**
+ * Upsert : save or update instances of T to the database.
+ *
+ * @param <T> the type to upsert.
+ */
+public class Upsert<T extends Yopable> {
+}
