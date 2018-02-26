9e4033fafd4f10783a0fb2ff07d43d699587221e
hdubourg <hdubourg@gmail.com>
2018-02-26 16:11:56 +0000

    [ORM] [MAPPER] [JAVADOC] Documentation of the Mapper : - Explicitly say when a method iterates over a ResultSet or NOT - Adding some TODO about cycle breaking and first level cache

--- src/main/java/org/yop/orm/map/Mapper.java
+++ src/main/java/org/yop/orm/map/Mapper.java
@@ -19,11 +19,25 @@ import java.util.*;
 
 /**
  * Maps results of an SQL query to a target class.
+ * <br>
+ * <b>This mapper is pretty basic for now</b>
+ * <br>
+ * TODO : First level cache ?
+ * TODO : Robust cycle breaking ?
  */
 public class Mapper {
 
 	private static final String SEPARATOR = Context.SQL_SEPARATOR;
 
+	/**
+	 * Map the results of an SQL SELECT request on to a target class.
+	 * <br>
+	 * <b>⚠⚠⚠ This method iterates over the resultset ! ⚠⚠⚠</b>
+	 * @param results the SQL query results (resultset + query + parameters...)
+	 * @param clazz   the target class
+	 * @param <T>     the target type
+	 * @return a set of T, read from the result set
+	 */
 	public static <T extends Yopable> Set<T> map(Results results, Class<T> clazz) {
 		try {
 			return map(results, clazz, clazz.getSimpleName());
@@ -34,6 +48,19 @@ public class Mapper {
 		}
 	}
 
+	/**
+	 * Map the results of an SQL SELECT request on to a target class, starting from a root context.
+	 * <br>
+	 * <b>⚠⚠⚠ This method iterates over the resultset ! ⚠⚠⚠</b>
+	 * @param results the results of the query
+	 * @param clazz   the target class
+	 * @param context the root context (mostly, the simple name of the target class)
+	 * @param <T>     the target type
+	 * @return a set of T, read from the result set
+	 * @throws IllegalAccessException could not read a field
+	 * @throws InstantiationException could not instantiate a target element
+	 * @throws SQLException           error reading the resultset
+	 */
 	private static <T extends Yopable> Set<T> map(
 		Results results,
 		Class<T> clazz,
@@ -51,6 +78,20 @@ public class Mapper {
 		return out;
 	}
 
+	/**
+	 * Map simple fields from a Resultset line for a given context.
+	 * <br>
+	 * This method iterates over the @Column fields of the target and search for data in the resultset entry.
+	 * <br>
+	 * <b>⚠⚠⚠ This method DOES NOT iterate over the resultset ! ⚠⚠⚠</b>
+	 * @param results the SQL query results
+	 * @param element the target element
+	 * @param context the target element context
+	 * @param <T> the target type
+	 * @return the input element, for chaining purposes
+	 * @throws SQLException           an error occured reading the resultset
+	 * @throws IllegalAccessException could not access a field on the target instance
+	 */
 	@SuppressWarnings("unchecked")
 	private static <T> T mapSimpleFields(
 		Results results,
@@ -78,7 +119,12 @@ public class Mapper {
 	}
 
 	/**
-	 * Map an enum value onto a field of an element
+	 * Map an enum value onto a field of an element.
+	 * <br>
+	 * This method simply search the value of the field in the result set, the enum strategy from @Column
+	 * and set the field value if some data was found in the resultset.
+	 * <br>
+	 * <b>⚠⚠⚠ This method DOES NOT iterate over the resultset ! ⚠⚠⚠</b>
 	 * @param resultSet  the resultset to read
 	 * @param enumField  the enum field. TYPE MUST BE ENUM
 	 * @param columnName the column name to read in the result set
@@ -106,11 +152,10 @@ public class Mapper {
 		try {
 			switch (strategy) {
 				case NAME:
-					enumField.set(element, Enum.valueOf(enumType, String.valueOf(value))
-					);
+					enumField.set(element, Enum.valueOf(enumType, String.valueOf(value)));
 					break;
 				case ORDINAL:
-					// Just in case an ordinal is stored as a string... A bit preposterous !
+					// Integer.valueOf(Objects.toString(val)) → ordinal is stored as a string... A bit preposterous !
 					enumField.set(element, enumType.getEnumConstants()[Integer.valueOf(Objects.toString(value))]);
 					break;
 				default:
@@ -123,6 +168,34 @@ public class Mapper {
 		}
 	}
 
+	/**
+	 * Map the relation fields of the target instance from the resultset entry.
+	 * <br>
+	 * This is where it gets a bit tricky. For every @JoinTable field of the target element :
+	 * <ol>
+	 *     <li>build a new context : context = context→fieldName</li>
+	 *     <li>
+	 *         check field type :
+	 *         <ul>
+	 *             <li>Yopable → new instance and map data</li>
+	 *             <li>Collection → check if new instance or if already exists, map and add to the collection</li>
+	 *             <li>Other → {@link YopMappingException}, unsupported</li>
+	 *         </ul>
+	 *     </li>
+	 *     <li>Recurse on the @JoinTable fields of the related instance</li>
+	 * </ol>
+	 * <br>
+	 * <b>⚠⚠⚠ This method DOES NOT iterate over the resultset ! ⚠⚠⚠</b>
+	 * @param results the results of the SQL SELECT query
+	 * @param element the target element
+	 * @param context the target element context
+	 * @param <T> the target type
+	 * @return the target instance, for chaining purposes
+	 * @throws IllegalAccessException could not read a field
+	 * @throws InstantiationException could not instantiate a target element
+	 * @throws SQLException           error reading the resultset
+	 * @throws YopMappingException    Incorrect mapping. Mostly a non Yopable/Collection of Yopable relationship.
+	 */
 	@SuppressWarnings("unchecked")
 	private static <T> T mapRelationFields(
 		Results results,
@@ -168,6 +241,15 @@ public class Mapper {
 		return element;
 	}
 
+	/**
+	 * Check if there is some data for the given context or not.
+	 * <b>⚠⚠⚠ This method DOES NOT iterate over the resultset ! ⚠⚠⚠</b>
+	 * @param results     the resultset entry to check
+	 * @param context     the context to check
+	 * @param targetClass the target class
+	 * @return true if there are no column AND no data for the given context
+	 * @throws SQLException error reading the resultset
+	 */
 	private static boolean noContext(
 		Results results,
 		String context,
@@ -194,17 +276,41 @@ public class Mapper {
 		return true;
 	}
 
+	/**
+	 * Check for an element in the given collection. Add to the context if does not exist.
+	 * <br>
+	 * TODO : First-level cache ?
+	 * @param element  the element to find
+	 * @param elements the elements to check into
+	 * @param <T> the target type
+	 * @return the found element or the input element after it is added in the collection
+	 */
 	private static <T extends Yopable> T cycleBreaker(T element, Collection<T> elements) {
 		T checked = elements.stream().filter(element::equals).findAny().orElse(element);
 		elements.add(checked);
 		return checked;
 	}
 
+	/**
+	 * Read the target type of a Collection relationship.
+	 * <br>
+	 * Example : {@code ArrayList<Pojo> → Pojo.class}
+	 * @param field the field to read
+	 * @param <T> the target type
+	 * @return the target class
+	 */
 	@SuppressWarnings("unchecked")
 	private static <T> Class<T> getRelationFieldType(Field field) {
 		return (Class<T>) ((ParameterizedType) field.getGenericType()).getActualTypeArguments()[0];
 	}
 
+	/**
+	 * Check if a given column is present in the resultset
+	 * @param rsmd       the resultset metadata
+	 * @param columnName the column name
+	 * @return true if the column is present in the resultset
+	 * @throws SQLException error reading the resultset
+	 */
 	private static boolean hasColumn(ResultSetMetaData rsmd, String columnName) throws SQLException {
 		int columns = rsmd.getColumnCount();
 		for (int x = 1; x <= columns; x++) {
