fdc78c02c1699b6ddc671f048d1214bdd2ac985f
hdubourg <hdubourg@gmail.com>
2018-03-07 15:20:31 +0000

    [ORM] [TEST] [SUPPLYCHAIN] Adding a new organisation and warehouses.

--- src/test/java/org/yop/orm/supplychain/SupplyChainTest.java
+++ src/test/java/org/yop/orm/supplychain/SupplyChainTest.java
@@ -3,8 +3,8 @@ package org.yop.orm.supplychain;
 import org.junit.Assert;
 import org.junit.Test;
 import org.yop.orm.DBMSSwitch;
-import org.yop.orm.query.Select;
-import org.yop.orm.query.Upsert;
+import org.yop.orm.evaluation.Operator;
+import org.yop.orm.query.*;
 import org.yop.orm.supplychain.model.Organisation;
 import org.yop.orm.supplychain.model.Warehouse;
 
@@ -26,33 +26,90 @@ public class SupplyChainTest extends DBMSSwitch {
 
 	@Test
 	public void testCreateOrganisation() throws SQLException, ClassNotFoundException {
-		Connection connection = this.getConnection();
-		connection.setAutoCommit(false);
+		try (Connection connection = this.getConnection()) {
+			connection.setAutoCommit(false);
 
-		Organisation organisation = new Organisation();
-		organisation.setName("world company & Co");
-		organisation.setSomeDummyFloat(3.1415F);
+			Organisation organisation = new Organisation();
+			organisation.setName("world company & Co");
+			organisation.setSomeDummyFloat(3.1415F);
 
-		Upsert.from(Organisation.class).checkNaturalID().onto(organisation).execute(connection);
-		connection.commit();
+			Upsert.from(Organisation.class).checkNaturalID().onto(organisation).execute(connection);
+			connection.commit();
 
-		Set<Organisation> organisations = Select.from(Organisation.class).joinAll().execute(connection);
-		Assert.assertEquals(1, organisations.size());
-		Assert.assertEquals(organisation, organisations.iterator().next());
+			Set<Organisation> organisations = Select.from(Organisation.class).joinAll().execute(connection);
+			Assert.assertEquals(1, organisations.size());
+			Assert.assertEquals(organisation, organisations.iterator().next());
 
-		Warehouse warehouse = new Warehouse();
-		warehouse.setActive(true);
-		warehouse.setAddress("1337 leet Road");
-		warehouse.setCapacity(456724234234L);
-		organisation.getWarehouses().add(warehouse);
+			Warehouse warehouse = new Warehouse();
+			warehouse.setActive(true);
+			warehouse.setAddress("1337 leet Road");
+			warehouse.setCapacity(456724234234L);
+			organisation.getWarehouses().add(warehouse);
 
-		Upsert.from(Organisation.class).checkNaturalID().onto(organisation).joinAll().execute(connection);
-		connection.commit();
+			Upsert.from(Organisation.class).checkNaturalID().onto(organisation).joinAll().execute(connection);
+			connection.commit();
 
-		organisations = Select.from(Organisation.class).joinAll().execute(connection);
-		Assert.assertEquals(1, organisations.size());
-		Assert.assertEquals(organisation, organisations.iterator().next());
-		Assert.assertEquals(organisation.getWarehouses(), organisations.iterator().next().getWarehouses());
+			organisations = Select.from(Organisation.class).joinAll().execute(connection);
+			Assert.assertEquals(1, organisations.size());
+			Assert.assertEquals(organisation, organisations.iterator().next());
+			Assert.assertEquals(organisation.getWarehouses(), organisations.iterator().next().getWarehouses());
+
+			// Let's add a lot of warehouses !
+			for (int i = 0; i < 150; i++) {
+				warehouse = new Warehouse();
+				warehouse.setActive(i % 2 == 0);
+				warehouse.setAddress(i + " My warehouses avenue");
+				warehouse.setCapacity(i * 100);
+				organisation.getWarehouses().add(warehouse);
+			}
+			Upsert.from(Organisation.class).checkNaturalID().onto(organisation).joinAll().execute(connection);
+			connection.commit();
+
+			// Fetch a transient relation ! (Warehouse → Organisation)
+			organisations = Select
+				.from(Organisation.class)
+				.join(JoinSet.to(Organisation::getWarehouses).join(Join.to(Warehouse::getOwner)))
+				.execute(connection);
+			Assert.assertEquals(1, organisations.size());
+			Assert.assertEquals(organisation, organisations.iterator().next());
+			Assert.assertEquals(151, organisations.iterator().next().getWarehouses().size());
+			Assert.assertEquals(organisation.getWarehouses(), organisations.iterator().next().getWarehouses());
+			Assert.assertEquals(organisation, organisations.iterator().next().getWarehouses().iterator().next().getOwner());
+
+			// Deleting a warehouse
+			organisation.getWarehouses().remove(0);
+			Upsert.from(Organisation.class).checkNaturalID().onto(organisation).joinAll().execute(connection);
+			organisations = Select.from(Organisation.class)
+				.join(JoinSet.to(Organisation::getWarehouses).join(Join.to(Warehouse::getOwner)))
+				.execute(connection);
+			Assert.assertEquals(1, organisations.size());
+			Assert.assertEquals(organisation, organisations.iterator().next());
+			Assert.assertEquals(150, organisations.iterator().next().getWarehouses().size());
+			Assert.assertEquals(organisation.getWarehouses(), organisations.iterator().next().getWarehouses());
+			Assert.assertEquals(organisation, organisations.iterator().next().getWarehouses().iterator().next().getOwner());
+
+			// Adding a new organisation and warehouses
+			organisation = new Organisation();
+			organisation.setName("Evil & Co");
+			organisation.setSomeDummyFloat(2.718F);
+
+			warehouse = new Warehouse();
+			warehouse.setActive(false);
+			warehouse.setAddress("17 Yolo Avenue");
+			warehouse.setCapacity(-3);
+			organisation.getWarehouses().add(warehouse);
+
+			Upsert.from(Organisation.class).joinAll().onto(organisation).execute(connection);
+			connection.commit();
+			Set<Warehouse> warehouses = Select
+				.from(Warehouse.class)
+				.where(Where.compare(Warehouse::getAddress, Operator.LIKE, "%Yolo%"))
+				.join(Join.to(Warehouse::getOwner))
+				.execute(connection);
+			Assert.assertEquals(1, warehouses.size());
+			Assert.assertEquals(warehouse, warehouses.iterator().next());
+			Assert.assertEquals(organisation, warehouses.iterator().next().getOwner());
+		}
 	}
 
 }
