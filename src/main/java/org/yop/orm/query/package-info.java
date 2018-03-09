b936a9943ffd142460636e303f9a2914137abd99
hdubourg <hdubourg@gmail.com>
2018-03-09 12:04:57 +0000

    [ORM] [QUERY] [JAVADOC] package-info precisions for the 'query' package.

--- src/main/java/org/yop/orm/query/package-info.java
+++ src/main/java/org/yop/orm/query/package-info.java
@@ -1,5 +1,5 @@
 /**
- * Query management.
+ * Query management : you'll find all you need here to CRUD your Yopable objects.
  * <br>
  * Yop aims at provinding a way to build requests that look like SQL and takes advantage of method references.
  */
