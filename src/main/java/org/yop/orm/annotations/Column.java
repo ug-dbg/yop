3a8fbf13dcbd923e1d1893d08c52574aef296ec5
hdubourg <hdubourg@gmail.com>
2018-03-13 17:19:30 +0000

    [ORM] [ANNOTATIONS] [COLUMN] [JAVADOC] Documenting @Column. This is pretty important !

--- src/main/java/org/yop/orm/annotations/Column.java
+++ src/main/java/org/yop/orm/annotations/Column.java
@@ -8,16 +8,38 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Link a field to a {@link Table} column.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface Column {
-    enum LengthStrategy {CUT, EXCEPTION, NONE}
-    enum EnumStrategy {NAME, ORDINAL}
+	/**
+	 * Strategy when a field value is too long for the column {@link #length()}.
+	 */
+	enum LengthStrategy {CUT, EXCEPTION, NONE}
 
-    String name();
-    int length() default 50;
-    boolean not_null() default false;
-    LengthStrategy length_strategy() default LengthStrategy.NONE;
-    EnumStrategy enum_strategy() default EnumStrategy.NAME;
-    Class<? extends ITransformer> transformer() default VoidTransformer.class;
+	/** How enums are serialized ? {@link Enum#name()} or {@link Enum#ordinal()} */
+	enum EnumStrategy {NAME, ORDINAL}
+
+	/** Be careful to avoid picking a name that your DBMS forbids ! */
+	String name();
+
+	/** Column max length. For now it is only used when creating tables */
+	int length() default 50;
+
+	/** If true, a 'NOT NULL' constraint will be added when creating the column. No further check is done. */
+	boolean not_null() default false;
+
+	/**
+	 * What to do when a field is too long ?
+	 * For now, length strategy is only implemented with {@link org.yop.orm.transform.AbbreviateTransformer}
+	 */
+	LengthStrategy length_strategy() default LengthStrategy.NONE;
+
+	/**If this field is an enum : use {@link Enum#name()} or {@link Enum#ordinal()} to serialize */
+	EnumStrategy enum_strategy() default EnumStrategy.NAME;
+
+	/** A transformer that will be used in two ways : Java → SQL and SQL → Java */
+	Class<? extends ITransformer> transformer() default VoidTransformer.class;
 }
