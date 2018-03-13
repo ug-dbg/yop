c928dd6937d8d4db2d7b625e3dac1f460e21ad14
hdubourg <hdubourg@gmail.com>
2018-03-13 17:20:40 +0000

    [ORM] [ANNOTATIONS] [ID] [JAVADOC] Documenting @Id. This is pretty important !

--- src/main/java/org/yop/orm/annotations/Id.java
+++ src/main/java/org/yop/orm/annotations/Id.java
@@ -5,6 +5,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Mark a field as the technical ID.
+ * <br>
+ * Yop requires an unique Id field of type {@link Long}.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface Id {
