8175cfc7dc0576454388825ef24ad33936b02c18
hdubourg <hdubourg@gmail.com>
2018-03-13 19:24:47 +0000

    [ORM] [ANNOTATIONS] [TABLE] [JAVADOC] Documenting @Table. This is pretty important !

--- src/main/java/org/yop/orm/annotations/Table.java
+++ src/main/java/org/yop/orm/annotations/Table.java
@@ -5,9 +5,15 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Link a Yopable object to an SQL table.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface Table {
-    String name();
-    String schema() default "";
+	/** Table name.  Be careful to avoid picking a name that your DBMS forbids ! */
+	String name();
+
+	/** Schema name. Does not really work for now. Please do not use. */
+	String schema() default "";
 }
