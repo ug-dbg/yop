30881ceaf82739f6201b532722e2120b3fdbac35
hdubourg <hdubourg@gmail.com>
2018-03-13 17:23:50 +0000

    [ORM] [ANNOTATIONS] [JOIN TABLE] [JAVADOC] Documenting @JoinTable. This is pretty important !

--- src/main/java/org/yop/orm/annotations/JoinTable.java
+++ src/main/java/org/yop/orm/annotations/JoinTable.java
@@ -5,13 +5,28 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * A relation to another Yopable object.
+ * <br>
+ * Yop makes no difference between One-to-One, One-to-Many and Many-to-Many : yop always require a join table.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface JoinTable {
-    String schema() default "";
-    String table();
-    String sourceColumn();
-    String targetColumn();
-    String sourceForeignKey() default "";
-    String targetForeignKey() default "";
+	String schema() default "";
+
+	/** join table name */
+	String table();
+
+	/** join table source column (representing this class) */
+	String sourceColumn();
+
+	/** join table target column (representing the relation target class) */
+	String targetColumn();
+
+	/** join table foreign key to this class name (used when generating tables) */
+	String sourceForeignKey() default "";
+
+	/** join table foreign key to the target class name (used when generating tables) */
+	String targetForeignKey() default "";
 }
