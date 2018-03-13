c9dce2d5df283d3f42293070b170eaad00755cf8
hdubourg <hdubourg@gmail.com>
2018-03-13 19:27:35 +0000

    [ORM] [ANNOTATIONS] [NATURALID] [JAVADOC] Documenting @NaturalId. This is pretty important !

--- src/main/java/org/yop/orm/annotations/NaturalId.java
+++ src/main/java/org/yop/orm/annotations/NaturalId.java
@@ -1,11 +1,18 @@
 package org.yop.orm.annotations;
 
+import org.yop.orm.model.Yopable;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Natural ID : one or severa fields on a class can be marked as an SQL unicity constraint.
+ * <br>
+ * This annotation is then used at runtime when the user explicitely needs it.
+ * For instance see {@link org.yop.orm.query.Where#naturalID(Yopable)}.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
-public @interface NaturalId {
-}
+public @interface NaturalId {}
