b246935495124666fdf3015411b6a9adb084d338
hdubourg <hdubourg@gmail.com>
2018-03-17 23:48:49 +0100

    [PACKAGE INFO] [ORG.YOP.ORM.ADAPTER.JDBC] Documentation of the default JDBC implementation of the Connection/Request/Cursor adapter pattern.

---
+++ src/main/java/org/yop/orm/sql/adapter/jdbc/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * SQL Connection/Request/Cursor <b>JDBC implementation</b>.
+ * <br>
+ * You will find in this package the 3 implementations for JDBC so YOP can work :-)
+ */
+package org.yop.orm.sql.adapter.jdbc;
