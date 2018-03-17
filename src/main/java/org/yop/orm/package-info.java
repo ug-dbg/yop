0cb9d2aed064cef938d7ca86b87132a0c22cc0cf
hdubourg <hdubourg@gmail.com>
2018-03-17 23:24:02 +0100

    [PACKAGE INFO] [ORG.YOP.ORM] Documentation of the root package.

---
+++ src/main/java/org/yop/orm/package-info.java
@@ -0,0 +1,33 @@
+/**
+ * <p>
+ *     <b>Welcome to YOP :-)</b>
+ * </p>
+ * <br>
+ * YOP is a dirty DIY Object-Relational Mapping tool
+ * whose syntax aims at being SQL-like so you can write inlined requests in Java such as :
+ * <br>
+ * {@code
+ *  Upsert
+ *   .from(Pojo.class)
+ *   .onto(newPojo)
+ *   .join(JoinSet.to(Pojo::getJopos).join(Join.to(Jopo::getPojo)))
+ *   .join(JoinSet.to(Pojo::getOthers))
+ *   .checkNaturalID()
+ *   .execute(connection);
+ * }
+ * <br>
+ * <br>
+ * Its core principles are built on 4 constraints :
+ * <ul>
+ *     <li>[1 data object ↔ 1 table] and [1 relation ↔ 1 table]</li>
+ *     <li>Deal with acyclic graphs of data only</li>
+ *     <li>There can be cycles in the Java objects data graph but :
+ *          <ul>
+ *              <li>they must be 'cut' using 'transient' keyword</li>
+ *              <li>transient relations must be explicitly invoked in queries</li>
+ *          </ul>
+ *     </li>
+ * </ul>
+ * YOP also tries to write very basic queries so it is not DBMS dependent. (Ha ha ha)
+ */
+package org.yop.orm;
