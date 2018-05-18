a24c600811e83aa01ee33fc930c06424077af04f
hdubourg <hdubourg@gmail.com>
2018-05-18 16:26:44 +0200

    [BATCH] [JAVADOC] package info file for "org.yop.orm.query.batch"

---
+++ src/main/java/org/yop/orm/query/batch/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Do some CRUD with batch queries.
+ * <br>
+ * For now {@link org.yop.orm.query.batch.BatchUpsert} exposes an API similar to {@link org.yop.orm.query.Upsert}
+ * but uses {@link org.yop.orm.sql.BatchQuery}.
+ */
+package org.yop.orm.query.batch;
