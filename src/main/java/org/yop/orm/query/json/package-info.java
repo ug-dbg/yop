10be482ef1cfadedb047f3fa679452be399731d6
hdubourg <hdubourg@gmail.com>
2018-05-18 16:26:33 +0200

    [JSON] [JAVADOC] package info file for "org.yop.orm.query.json"

---
+++ src/main/java/org/yop/orm/query/json/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Yop can serialize to JSON using the same join directives to control the relation propagation.
+ * <br>
+ * See the {@link org.yop.orm.query.json.JSON} API.
+ */
+package org.yop.orm.query.json;
