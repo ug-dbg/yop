a9a591cf2f82e324dbaada2fa1068496dea2410e
hdubourg <hdubourg@gmail.com>
2018-05-22 12:11:11 +0200

    [JOIN COLUMN] [ANNOTATION] [JAVADOC] Some documentation about the local and remote fields of @JoinColumn.

--- src/main/java/org/yop/orm/annotations/JoinColumn.java
+++ src/main/java/org/yop/orm/annotations/JoinColumn.java
@@ -22,11 +22,17 @@ import java.lang.annotation.Target;
  *     <li>unidirectional → local or remote is set</li>
  *     <li>bidirectional → local and remote are set</li>
  * </ul>
+ * <b>
+ *     There is no fallback when neither {@link #local()} nor {@link #remote()} is set !
+ *     Please either set one of them or both !
+ * </b>
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface JoinColumn {
-	/** */
+	/** The name of the join column on the local table (1 → 1 or N → 1) */
 	String local()  default "";
+
+	/** The name of the join column on the target table (1 → 1 or 1 → N) */
 	String remote() default "";
 }
