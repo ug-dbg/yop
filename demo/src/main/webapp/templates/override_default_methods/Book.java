0cf5c022ee69ea28a47b7cc012f37c92112aa821
hdubourg <hdubourg@gmail.com>
2018-10-26 17:41:33 +0200

    [DEMO] [EXAMPLES] Three default examples for now, using the same 'Book' model : - default behavior + init method - overring some default behavior + init method - using path ref in comparisons + init method

---
+++ demo/src/main/webapp/templates/override_default_methods/Book.java
@@ -0,0 +1,210 @@
+import java.time.LocalDate;
+import java.util.*;
+
+import org.yop.orm.evaluation.*;
+import org.yop.orm.model.Yopable;
+import org.yop.orm.annotations.*;
+import org.yop.orm.query.*;
+import org.yop.orm.sql.adapter.IConnection;
+import org.yop.rest.annotations.*;
+
+/**
+ * In this example we override some of the default REST methods of YOP.
+ * <ul>
+ *     <li>GET → get all the books with non null date of purchase</li>
+ *     <li>DELETE by ID → delete and return the deleted object</li>
+ * </ul>
+ */
+@Rest(
+	path="book",
+	summary = "Rest resource for books !",
+	description = "is both a usually portable physical object "
+		+ "and the body of immaterial representations "
+		+ "or intellectual object whose material signs"
+)
+@Table(name="book")
+public class Book implements Yopable {
+
+	@Id
+	@Column(name="id")
+	private Long id;
+
+	@Column(name="title")
+	private String title;
+
+	@Column(name = "purchase_date")
+	private LocalDate purchaseDate;
+
+	@JoinTable(table = "rel_book_author", sourceColumn = "id_book", targetColumn = "id_author")
+	private Collection<Author> authors = new ArrayList<>();
+
+	@JoinColumn(remote = "id_book")
+	private List<Chapter> chapters = new ArrayList<>();
+
+	private Book(){}
+
+	public Book(String title, LocalDate purchaseDate) {
+		this.title = title;
+		this.purchaseDate = purchaseDate;
+	}
+
+	public Book(String title, LocalDate purchaseDate, Author... authors) {
+		this.title = title;
+		this.purchaseDate = purchaseDate;
+		this.authors.addAll(Arrays.asList(authors));
+	}
+
+	public Collection<Author> getAuthors() {
+		return this.authors;
+	}
+
+	public List<Chapter> getChapters() {
+		return this.chapters;
+	}
+
+	public LocalDate getPurchaseDate() {
+		return this.purchaseDate;
+	}
+
+	@Rest(description = "get books that are already purchased")
+	public static Set<Book> get(IConnection connection) {
+		return Select.from(Book.class).where(Where.isNotNull(Book::getPurchaseDate)).execute(connection);
+	}
+
+	@Rest(path = "{id}", methods = "DELETE", description = "delete books and return deleted elements")
+	public static Book delete(
+		IConnection connection,
+		@PathParam(name = "id") Long id,
+		@RequestParam(name = "joinAll", required = true) Boolean joinAll) {
+		Select<Book> select = Select.from(Book.class).where(Where.id(id));
+		if (joinAll) {
+			select.joinAll();
+		}
+		Book toDelete = select.uniqueResult(connection);
+		Delete<Book> delete = Delete.from(Book.class).where(Where.id(id));
+		if (joinAll) {
+			delete.joinAll();
+		}
+		delete.executeQueries(connection);
+		return toDelete;
+	}
+
+	@Rest(path = "init", description = "insert some books/authors/chapters")
+	public static Object init(IConnection connection) {
+		// Delete everything !
+		Delete.from(Book.class).joinAll().executeQueries(connection);
+
+		Author ug    = new Author("Ug");
+		Author roger = new Author("Roger");
+		Author mitch = new Author("Mitch");
+		Author bhl   = new Author("BHL");
+
+		Book yopManual = new Book("Yop unwritten manual", LocalDate.now(), ug, mitch);
+		for (int i = 1; i <= 42; i++) {
+			yopManual.chapters.add(new Chapter("Chapter #" + i, i));
+		}
+
+		Book yopForDummies = new Book("Yop for dummies and/or my mom", LocalDate.of(1970, 1, 1), roger);
+		for (int i = 1; i <= 42; i++) {
+			yopForDummies.chapters.add(new Chapter("Chapter n°" + i, i));
+		}
+
+		Book dullBook = new Book("Garter stitches through the ages", LocalDate.of(1234, 5, 6), mitch, bhl, roger);
+		for (int i = 1; i <= 175; i++) {
+			dullBook.chapters.add(new Chapter("Dull chapter #" + i, i));
+		}
+
+		// Insert the books. Authors and chapters will be inserted thanks to 'joinAll'.
+		List<Book> books = Arrays.asList(yopManual, yopForDummies, dullBook);
+		Upsert.from(Book.class).joinAll().onto(books).execute(connection);
+		return books;
+	}
+}
+
+@Rest(path="author")
+@Table(name="author")
+class Author implements Yopable {
+
+	@Id
+	@Column(name="id")
+	private Long id;
+
+	@Column(name="name")
+	private String name;
+
+	@YopTransient
+	@JoinTable(table = "rel_book_author", sourceColumn = "id_author", targetColumn = "id_book")
+	private List<Book> books = new ArrayList<>();
+
+	private Author() {}
+
+	public Author(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public Long getId() {
+		return this.id;
+	}
+
+	@Override
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public List<Book> getBooks() {
+		return this.books;
+	}
+}
+
+@Rest(path="chapter")
+@Table(name="chapter")
+class Chapter implements Yopable {
+
+	@Id
+	@Column(name="id")
+	private Long id;
+
+	@Column(name="name")
+	private String name;
+
+	@Column(name = "chapterNumber")
+	private Integer chapterNumber;
+
+	@YopTransient
+	@JoinColumn(local = "id_book")
+	private Book book;
+
+	private Chapter() {}
+
+	public Chapter(String name, Integer chapterNumber) {
+		this.name = name;
+		this.chapterNumber = chapterNumber;
+	}
+
+	@Override
+	public Long getId() {
+		return this.id;
+	}
+
+	@Override
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public Integer getChapterNumber() {
+		return this.chapterNumber;
+	}
+
+	public Book getBook() {
+		return this.book;
+	}
+}
---
+++ demo/src/main/webapp/templates/path_ref/Book.java
@@ -0,0 +1,225 @@
+import java.time.LocalDate;
+import java.util.*;
+
+import org.yop.orm.evaluation.*;
+import org.yop.orm.model.Yopable;
+import org.yop.orm.annotations.*;
+import org.yop.orm.query.*;
+import org.yop.orm.sql.adapter.IConnection;
+import org.yop.rest.annotations.*;
+
+/**
+ * In this class, authors have a birth date and books a purchase date.
+ * <br>
+ * The init method inserts some books, authors and chapters.
+ * <br><br>
+ * A subtle {@link #timeParadox(IConnection, Boolean)} method
+ * tries to find books which were purchased before the birth of any of their authors ;-)
+ * <br>
+ * This method uses a {@link Path} in the {@link Comparison}
+ * to build a path from {@link Book} to {@link Author#getBirthDate()}.
+ */
+@Rest(
+	path="book",
+	summary = "Rest resource for books !",
+	description = "is both a usually portable physical object "
+		+ "and the body of immaterial representations "
+		+ "or intellectual object whose material signs"
+)
+@Table(name="book")
+public class Book implements Yopable {
+
+	@Id
+	@Column(name="id")
+	private Long id;
+
+	@Column(name="title")
+	private String title;
+
+	@Column(name = "purchase_date")
+	private LocalDate purchaseDate;
+
+	@JoinTable(table = "rel_book_author", sourceColumn = "id_book", targetColumn = "id_author")
+	private Collection<Author> authors = new ArrayList<>();
+
+	@JoinColumn(remote = "id_book")
+	private List<Chapter> chapters = new ArrayList<>();
+
+	private Book(){}
+
+	public Book(String title, LocalDate purchaseDate) {
+		this.title = title;
+		this.purchaseDate = purchaseDate;
+	}
+
+	public Book(String title, LocalDate purchaseDate, Author... authors) {
+		this.title = title;
+		this.purchaseDate = purchaseDate;
+		this.authors.addAll(Arrays.asList(authors));
+	}
+
+	public Collection<Author> getAuthors() {
+		return this.authors;
+	}
+
+	public List<Chapter> getChapters() {
+		return this.chapters;
+	}
+
+	public String getTitle() {
+		return this.title;
+	}
+
+	public LocalDate getPurchaseDate() {
+		return this.purchaseDate;
+	}
+
+	@Rest(path = "time_paradox", description = "Find books purchased before the author is actually born !")
+	public static Set<Book> timeParadox(
+		IConnection connection,
+		@RequestParam(name = "joinAll") Boolean joinAll) {
+		Select<Book> select = Select
+			.from(Book.class)
+			.join(JoinSet.to(Book::getAuthors))
+			.where(Where.compare(
+				Book::getPurchaseDate,
+				Operator.LT,
+				Path.pathSet(Book::getAuthors).to(Author::getBirthDate)
+		));
+		if (joinAll != null && joinAll) {
+			select.joinAll();
+		}
+		return select.execute(connection);
+	}
+
+	@Rest(path = "init", description = "insert some books/authors/chapters")
+	public static Object init(IConnection connection) {
+		// Delete everything !
+		Delete.from(Book.class).joinAll().executeQueries(connection);
+
+		Author ug    = new Author("Ug", LocalDate.of(1984, 12, 8));
+		Author roger = new Author("Roger", LocalDate.of(1977, 7, 7));
+		Author mitch = new Author("Mitch", LocalDate.of(1900, 1, 1));
+		Author bhl   = new Author("BHL", LocalDate.of(1500, 1, 1));
+
+		Book yopManual = new Book("Yop unwritten manual", LocalDate.now(), ug, mitch);
+		for (int i = 1; i <= 42; i++) {
+			yopManual.chapters.add(new Chapter("Chapter #" + i, i));
+		}
+
+		Book yopForDummies = new Book("Yop for dummies and/or my mom and/or Roger", LocalDate.of(1970, 1, 1), roger);
+		for (int i = 1; i <= 42; i++) {
+			yopForDummies.chapters.add(new Chapter("Chapter n°" + i, i));
+		}
+
+		Book dullBook = new Book("Garter stitches through the ages", LocalDate.of(1234, 5, 6), mitch, bhl, roger);
+		for (int i = 1; i <= 175; i++) {
+			dullBook.chapters.add(new Chapter("Dull chapter #" + i, i));
+		}
+
+		// Insert the books. Authors and chapters will be inserted thanks to 'joinAll'.
+		List<Book> books = Arrays.asList(yopManual, yopForDummies, dullBook);
+		Upsert.from(Book.class).joinAll().onto(books).execute(connection);
+		return books;
+	}
+}
+
+@Rest(path="author")
+@Table(name="author")
+class Author implements Yopable {
+
+	@Id
+	@Column(name="id")
+	private Long id;
+
+	@Column(name="name")
+	private String name;
+
+	@Column(name="birth_date")
+	private LocalDate birthDate;
+
+	@YopTransient
+	@JoinTable(table = "rel_book_author", sourceColumn = "id_author", targetColumn = "id_book")
+	private List<Book> books = new ArrayList<>();
+
+	private Author() {}
+
+	public Author(String name) {
+		this.name = name;
+	}
+
+	public Author(String name, LocalDate birthDate) {
+		this.name = name;
+		this.birthDate = birthDate;
+	}
+
+	@Override
+	public Long getId() {
+		return this.id;
+	}
+
+	@Override
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public LocalDate getBirthDate() {
+		return this.birthDate;
+	}
+
+	public List<Book> getBooks() {
+		return this.books;
+	}
+}
+
+@Rest(path="chapter")
+@Table(name="chapter")
+class Chapter implements Yopable {
+
+	@Id
+	@Column(name="id")
+	private Long id;
+
+	@Column(name="name")
+	private String name;
+
+	@Column(name = "chapterNumber")
+	private Integer chapterNumber;
+
+	@YopTransient
+	@JoinColumn(local = "id_book")
+	private Book book;
+
+	private Chapter() {}
+
+	public Chapter(String name, Integer chapterNumber) {
+		this.name = name;
+		this.chapterNumber = chapterNumber;
+	}
+
+	@Override
+	public Long getId() {
+		return this.id;
+	}
+
+	@Override
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public Integer getChapterNumber() {
+		return this.chapterNumber;
+	}
+
+	public Book getBook() {
+		return this.book;
+	}
+}
