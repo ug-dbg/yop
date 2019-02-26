fbf3397fd2364532c3caff6ce15405c08124e902
hdubourg <hdubourg@gmail.com>
2019-02-26 12:17:40 +0100

    [XML] [PACKAGE INFO] Missing package info in orm.query.serialize.xml.

---
+++ src/main/java/org/yop/orm/query/serialize/xml/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Yop can serialize to XML using the same join directives to control the relation propagation.
+ * <br>
+ * See the {@link org.yop.orm.query.serialize.xml.XML} API.
+ */
+package org.yop.orm.query.serialize.xml;
